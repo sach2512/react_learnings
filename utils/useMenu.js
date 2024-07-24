080a348ab635fd1a06c5bc752745e7168c1a12ed
sachin <sachinjaing494@gmail.com>
2024-07-23 21:15:51 -0500

    updated fetch

--- utils/useMenu.js
+++ utils/useMenu.js
@@ -5,10 +5,11 @@ export const useMenu = (id) => {
     const [menu, setMenu] = useState([]);
 
     const getMenu = async () => {
-        alert(Menu_URL)
+       
         try {
             const response = await fetch(Menu_URL);
             const data = await response.json();
+            console.log(`the data is ${data}`);
             // Filter menu items based on the restaurant_id matching id from URL params
             const filteredMenu = data.filter(item => item.restaurant_id == id); // Use == to match string to number
             setMenu(filteredMenu);
